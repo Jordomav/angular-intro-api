ff590f86f640fbc956a1e3579096552dc5a56475
Jordan Mavrogeorge <jordomav@gmail.com>
2019-05-23 16:46:22 -0700

    update post entity

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,6 +1,32 @@
-import {Entity} from "typeorm";
+
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    Index,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    ManyToOne, JoinColumn
+} from "typeorm";
+import { User } from "./User";
 
 @Entity()
 export class Post {
+    @Index()
+    @PrimaryGeneratedColumn()
+    id: number;
 
+    @Index()
+    @Column({ unique: true })
+    body: string;
+
+    @ManyToOne(type => User, { eager: true })
+    @JoinColumn()
+    user: User;
+
+    @CreateDateColumn()
+    createdAd: string;
+
+    @UpdateDateColumn()
+    updatedAt: string;
 }
